f24ec99d7f398fa601bf7e4814959fee9b4442df
michael willy <michaellwillyy@gmail.com>
2025-12-14 10:22:02 -0300

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
 const api = axios.create({
-    baseURL: 'https://backend-login-production-ffad.up.railway./usuarios'
+    baseURL: 'https://backend-login-production-ffad.up.railway.app/usuarios'
 })
 export default api
